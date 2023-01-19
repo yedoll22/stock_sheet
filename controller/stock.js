04ba05bf82770a5251548135700a232d4e5d2ee9
yedoll <yedoll22@gmail.com>
2023-01-19 17:47:23 +0900

    modify controller

--- controller/stock.js
+++ controller/stock.js
@@ -1,12 +1,11 @@
 const { Ins, Outs, Sheets, Storages } = require("../models");
-const sheets = require("../models/sheets");
 const sequelize = require("sequelize");
 
 module.exports = {
   get: async (req, res) => {
     // try {
     const stockList = await Ins.findAll({
-      attributes: ["id", "date", "quantity", "createdAt"],
+      attributes: ["id", "date", "quantity"],
       include: [
         { model: Storages, attributes: ["name", "color_code"] },
         { model: Sheets, attributes: ["pattern", "type", "width", "height"] },
@@ -19,14 +18,29 @@ module.exports = {
   },
 
   post: async (req, res) => {
-    const { date, quantity, sheets_id, storages_id } = req.body;
-    await Ins.create({
-      date,
-      quantity,
-      sheets_id,
-      storages_id,
-      category: "입고",
-    });
-    res.status(200);
+    const { date, quantity, pattern, type, storages_id, category } = req.body;
+
+    try {
+      const id = await Sheets.findAll({
+        attributes: ["id"],
+        where: {
+          pattern,
+          type,
+        },
+      });
+
+      const sheets_id = id[0].dataValues.id;
+      await Ins.create({
+        date,
+        quantity,
+        sheets_id,
+        storages_id,
+        category,
+      });
+      return res.sendStatus(201);
+    } catch (err) {
+      console.log(err);
+      return res.sendStatus(500);
+    }
   },
 };

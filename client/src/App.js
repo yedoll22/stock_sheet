9c2e7b4b12d94c488ef05b10c2a0cb908904ff1d
yedoll22 <yedoll22@gmail.com>
2023-01-29 23:53:27 +0900

    meltilineTernary need to modify

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,27 @@
+import { useState } from 'react'
+
+const [isOpen, setIsOpen] = useState(false)
+
 function App() {
-  return <div>테스트</div>
+  const handleClick = () => {
+    setIsOpen(!isOpen)
+  }
+  return (
+    <div>
+      <div onClick={handleClick}> 클릭 </div>
+      {isOpen ? (
+        <div>
+          <ul>
+            <li>350IV</li>
+            <li>300ART</li>
+            <li>350B/W</li>
+          </ul>
+        </div>
+      ) : (
+        <div>클릭해주세요~</div>
+      )}
+    </div>
+  )
 }
 
 export default App
---
+++ client/src/components/dropdown.js
@@ -0,0 +1,15 @@
+const Dropdown = ({
+return (
+  <div>
+  <Dropdown>
+  <ul>
+  <li>350IV</li>
+  <li>300ART</li>
+  <li>350B/W</li>
+  </ul>
+  </Dropdown>
+  </div>
+)
+})
+
+export default Dropdown

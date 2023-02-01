8e7233f129d5e6247af0f58b708b183ad45f0d44
yedoll <yedoll22@gmail.com>
2023-02-01 17:53:26 +0900

    드롭다운추가

--- client/src/components/Dropdown.js
+++ client/src/components/Dropdown.js
@@ -1,8 +1,47 @@
+import { useState, useRef } from 'react'
+import { useLocation } from 'react-router-dom'
+
 function Dropdown() {
+  const [selected, setSelected] = useState('선택하세요.')
+  const [isOpen, setIsOpen] = useState(false)
+  const ref = useRef()
+
+  const { pathname } = useLocation()
+  let content = { title: '재질', text: ['350IV', '350B/W', '300ART'] }
+  if (pathname === '/storage')
+    content = {
+      title: '위치',
+      text: ['1층 위', '1층 밑', '소부실 앞', '인쇄실']
+    }
+
+  // const onclickHandler =
+
   return (
-    <div className="border rounded border-[#D5DBE2] w-1/3 h-7 items-center mb-7 flex justify-between p-2">
-      선택하세요
-      <img className="w-5 h-5" src="/images/Down arrow.png" alt="downArrow" />
+    <div className="mb-7 flex item-center relative">
+      <div className="font-bold mr-2">{content.title}</div>
+      <div
+        onClick={(e) => {
+          setIsOpen(!isOpen)
+          console.log(ref)
+        }}
+        className="border rounded border-[#D5DBE2] w-1/4 h-7 items-center flex justify-between pl-3 py-1"
+      >
+        {selected}
+        <img className="w-5 h-5" src="/images/Down arrow.png" alt="downArrow" />
+        {isOpen && (
+          <div className="absolute top-8 border border-gray-100 left-10 bg-white shadow-lg w-1/4 rounded py-2 px-[10px]">
+            {content.text.map((data) => (
+              <div
+                onClick={() => setSelected(data)}
+                key={data}
+                className="py-[10px] pl-4 hover:bg-[#e3f2fd]"
+              >
+                {data}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   )
 }
